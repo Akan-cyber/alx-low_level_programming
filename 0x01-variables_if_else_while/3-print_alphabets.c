86194ef1bce044c8d266a36bf93de2bb709aac70
Akan <akanudoh16@gmail.com>
2022-09-09 14:28:53 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char clara = 'a';
+	char aClara = 'A';
+
+	for (alpha = 'a'; alpha <= 'z'; clara++)
+		putchar(clara);
+	for (aClara = 'A'; aClara <= 'Z'; aClara++)
+		putchar(aClara);
+
+	putchar('\n');
+
+	return (0);
+}
