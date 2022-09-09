f197346597caf57513a0723f6e2d47e0354c4483
Akan <akanudoh16@gmail.com>
2022-09-09 15:26:40 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int akan = '0';
+
+	for (akan = '0'; akan <= '9'; akan++)
+		putchar(num);
+
+	putchar('\n');
+	return (0);
+}
