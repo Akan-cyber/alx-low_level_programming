fd8a6275a12cc61fb8476996acfe0af869e8c9a9
Akan <akanudoh16@gmail.com>
2022-09-09 14:22:48 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char clara = 'a';
+
+	for (clara = 'a'; clara <= 'z'; clara++)
+		putchar(clara);
+
+	putchar('\n');
+
+	return (0);
+}
