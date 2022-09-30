36d8624184d0cab14cf0c4c25c73b2180fa93bc6
Akan <akanudoh16@gmail.com>
2022-09-30 04:52:07 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
-#include <stdio.h>                                                                                                                   
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * main - Print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                  
-                                                                                                                                     
-int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-(void) argc;                                                                                                                         
-printf("%s\n", argv[0]);                                                                                                             
-                                                                                                                                     
-return (0);                                                                                                                          
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the name of the executable.
+ * @argc: argument count or size of arguments.
+ * @argv: argument vector
+ *
+ * Return: always 0.
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,6 @@
-int _putchar(char c);                                                                                                                
-int check_num(char *str);
+#ifndef MAIN.H
+#define MAIN.H
+
+int _putchar(char c);
+
+#endif
