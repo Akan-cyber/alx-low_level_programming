796b35880c7cec96e89fef6478ca6f1c35209057
Akan <akanudoh16@gmail.com>
2022-10-11 05:26:50 -0700

    TAsks

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,19 @@
+root@d84bfe4544ba:/alx-low_level_programming/0x0D-preprocessor# ls
+README.md
+root@d84bfe4544ba:/alx-low_level_programming/0x0D-preprocessor# cat > 0-object_like_macro.h
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+
+#define SIZE 1024
+
+#endif /* _0_OBJECT_LIKE_MACRO_H_ */
+^C
+root@d84bfe4544ba:/alx-low_level_programming/0x0D-preprocessor# ls
+0-object_like_macro.h  README.md
+root@d84bfe4544ba:/alx-low_level_programming/0x0D-preprocessor# cat > 1-pi.h
+#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+
+#endif /* _PI_H_ */
